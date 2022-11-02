a502c30fa608eb244a4d772ae818a9e3b9adbfda
toddferrante <tf0z@verizon.net>
2022-11-02 15:06:45 -0400

    Add files via upload

---
+++ DohPro-TF.bat
@@ -0,0 +1,14 @@
+ren *.lrv *.mp4
+SETLOCAL ENABLEDELAYEDEXPANSION
+for %%f in (GH*.MP4) do (
+set a=%%f
+set chap=!a:~2,2!
+set vid=!a:~4,4!
+ren %%f GH-!vid!-!chap!.MP4
+)
+for %%f in (GL*.MP4) do (
+set a=%%f
+set chap=!a:~2,2!
+set vid=!a:~4,4!
+ren %%f GL-!vid!-!chap!.MP4
+)
